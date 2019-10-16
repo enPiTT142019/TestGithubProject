3fa7ff515fbc575073928b7d29eeca3a9a9d9289
enPiT-P12 <f217029@ed.socu.ac.jp>
2019-10-16 16:50:15 +0900

    田坂匠2

--- app/src/main/java/com/socu/enpit/testgithubproject/MainActivity.kt
+++ app/src/main/java/com/socu/enpit/testgithubproject/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     // mashida
     override fun onCreate(savedInstanceState: Bundle?) {
         //田坂匠
-        //taskakk
+        //taskakkk
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //FujimotoAkihisa
