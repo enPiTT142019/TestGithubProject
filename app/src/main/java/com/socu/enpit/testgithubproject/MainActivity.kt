dec42a96d9710c16b8de83e381cb50836976a021
enPiT-P12 <f217029@ed.socu.ac.jp>
2019-10-16 15:50:04 +0900

    田坂匠

--- app/src/main/java/com/socu/enpit/testgithubproject/MainActivity.kt
+++ app/src/main/java/com/socu/enpit/testgithubproject/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //田坂匠
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
